4713759ecd4e4a531c16dd3701b547fc799db83b
Matthijs van Duin <matthijs@dutchdutch.com>
2016-05-09 18:07:24 +0200

    allow uio device specified by Path object

--- uio.py
+++ uio.py
@@ -22,7 +22,7 @@ class Uio:
         flags = O_RDWR | O_CLOEXEC
         if not blocking:
             flags |= O_NONBLOCK # for irq_recv
-        self._fd = os.open( path, flags )
+        self._fd = os.open( getattr( path, 'path', path ), flags )
 
         # check sysfs for available memory mappings
         dev = os.stat( self ).st_rdev
