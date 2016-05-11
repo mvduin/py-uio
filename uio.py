7f84f48a3de04d3a17e69712d0e29f776dfa5bcf
Matthijs van Duin <matthijsvanduin@gmail.com>
2016-05-11 05:54:26 +0200

    bit of cleanup, add support for parent region

--- uio.py
+++ uio.py
@@ -11,20 +11,32 @@ PAGE_MASK = -PAGE_SIZE
 
 class MemRegion:
     def __init__( rgn, uio, info ):
+        rgn.uio = uio
         rgn.index = int( re.fullmatch( r'map([0-9])', info.name ).group(1) )
+
         def getinfo( attr ):
             with (info/attr).open() as f:
                 return f.readline().rstrip()
+
+        # If no name has been supplied, the region gets an auto-generated name
+        # containing the full DT path.  These are useless, ignore them.
         rgn.name = getinfo( 'name' )
+        if rgn.name == '' or rgn.name[0] == '/':
+            rgn.name = None
+
+        # get memory region bounds
         rgn.address = int( getinfo('addr'), 0 )
         rgn.size = int( getinfo('size'), 0 )
         rgn.end = rgn.address + rgn.size
+
+        # size that can be actually mmap()ed
         if rgn.address & ~PAGE_MASK:
             rgn.mappable = 0   # not page-aligned, can't be mapped
         else:
             rgn.mappable = rgn.size & PAGE_MASK
+
+        # mmap() is done lazily when needed
         rgn._mmap = None
-        rgn.uio = uio
 
     def __contains__( rgn, child ):
         return child.address >= rgn.address and child.end <= rgn.end
@@ -58,12 +70,22 @@ class Uio:
     def fileno( self ):
         return self._fd
 
-    def __init__( self, path, blocking = True ):
+    def __init__( self, path, blocking=True, parent=None ):
+        path = Path( '/dev/uio', path )
+        self.path = path
+
         flags = O_RDWR | O_CLOEXEC
         if not blocking:
             flags |= O_NONBLOCK # for irq_recv
-        path = Path( '/dev/uio', path )
         self._fd = os.open( str(path), flags )
+
+        # parent memory region (if any)
+        if parent is not None:
+            if isinstance( parent, Uio ):
+                parent = parent.region()
+            elif isinstance( parent, MemRegion ):
+                raise TypeError
+        self.parent = parent
 
         # build path to sysfs dir for obtaining metadata
         dev = os.stat( self._fd ).st_rdev
@@ -71,13 +93,21 @@ class Uio:
         self.syspath = Path('/sys/dev/char', dev).resolve()
 
         # enumerate memory regions
+        # beware that if there are none, the dir is absent rather than empty
         self._regions = {}
         rgninfo = self.syspath/'maps';
         if rgninfo.is_dir():
             for info in rgninfo.iterdir():
                 rgn = MemRegion( self, info )
+
+                # sanity check
+                if parent is not None and rgn not in parent:
+                    raise RuntimeError( "memory region outside parent" )
+
+                # allow lookup by index or (if available) by name
                 self._regions[ rgn.index ] = rgn
-                self._regions[ rgn.name ] = rgn
+                if rgn.name:
+                    self._regions[ rgn.name ] = rgn
 
     def region( self, rgn=0 ):
         return self._regions[ rgn ]
@@ -86,8 +116,14 @@ class Uio:
         rgn = 0
         if type(args[0]) in (int,str):
             (rgn,*args) = args
-        return self.region( rgn ).map( *args )
+        rgn = self.region( rgn )
+        if self.parent is not None:
+            (struct,) = args
+            return self.parent.map( struct, rgn )
+        else:
+            return rgn.map( *args )
 
+    # TODO determine if the device has any irq
 
     def irq_enable( self ):
         os.write( self._fd, b'\x01\x00\x00\x00' )
