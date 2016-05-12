0bea1f2172eee7de2f81bf43a0e0a51fb1cac7b3
Matthijs van Duin <matthijsvanduin@gmail.com>
2016-05-12 11:23:54 +0200

    qwp example: verbose irq messages, enable index latch

--- qep-test.py
+++ qep-test.py
@@ -19,10 +19,25 @@ m.timer_maximum = FCK // 2 - 1  # 2 Hz
 m.timer_counter = 0
 
 m.io_config = 0
-m.ctr_config = 1 << 1 | 1 << 3 | 1 << 12
+m.ctr_config = 1 << 1 | 1 << 3 | 1 << 12 | 2 << 4
 m.irq_enabled = 0xffe;
 
 lastpos = -1
+
+irq_msgs = (
+        None,
+        "position counter error",
+        "quadrature decoder error",
+        "dir change",
+        "watchdog",
+        "underflow",
+        "overflow",
+        "compare load",
+        "compare match",
+        "strobe latch",
+        "index latch",
+        "timer",
+        )
 
 while True:
     pos = m.position
@@ -30,7 +45,14 @@ while True:
     m.irq_clear( irq )
 
     if irq & 0x7f6 or pos != lastpos:
-        print( "position {0:02d}, irq {1:012b}".format( pos, irq ) )
+        s = "pos {0:02d}".format( pos )
+        for bit,msg in enumerate(irq_msgs):
+            if not msg: continue
+            if irq & 1 << bit:
+                s += ", " + msg
+                if bit == 10:
+                    s += " {0:02d}".format( m.index_latch )
+        print( s )
         lastpos = pos
 
     qep.irq_enable()
