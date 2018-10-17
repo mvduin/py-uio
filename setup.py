a2958e66089fe6f44415b70874f487bf619bc500
Jeremy Stashluk <stashlukj@geophysical.com>
2018-10-17 14:18:17 +0000

    enable setuptools install

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+Setup file to manage package dependencies
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='py-uio',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src') + ['ti'],
+    py_modules=['devicetree', 'uio'],
+    version_format='{tag}.dev{commits}+{sha}',
+    setup_requires=['very-good-setuptools-git-version'],
+
+    # metadata
+    description="Userspace I/O in Python",
+    author="Matthijs van Duin",
+    url="https://github.com/mvdiuin/py-uio",
+)
