e10b0ed8614ee30b92d055b4fcfe9f015444a676
Matthijs van Duin <matthijsvanduin@gmail.com>
2018-10-24 06:48:35 +0200

    eqep: import comments
    also add an ld_compare field as alias for compare

--- src/uio/ti/eqep.py
+++ src/uio/ti/eqep.py
@@ -14,7 +14,7 @@ class EQep( ctypes.Structure ):
             #-------- position counter --------------------------------------------
             #
             ("position",        pos_t),     #<w
-            ("ld_position",     pos_t),     #rw  loaded into counter on strobe/index if enabled
+            ("ld_position",     pos_t),     #rw  loaded into position on strobe/index if enabled
             ("maximum",         pos_t),     #rw  = period - 1
             ("compare",         pos_t),     #rw  (may be ld_compare depending on config)
 
@@ -25,7 +25,7 @@ class EQep( ctypes.Structure ):
             #-------- unit timer (frequency measurement base) ---------------------
             #
             ("timer_counter",   tim_t),     #rw
-            ("timer_maximum",   tim_t),     #rw
+            ("timer_maximum",   tim_t),     #rw  = period - 1
 
             #-------- watchdog timer (motor stall detection) ----------------------
             #
@@ -37,15 +37,107 @@ class EQep( ctypes.Structure ):
 
             #-------- configuration -----------------------------------------------
             #
-            ("io_config",       ushort),    #rw
-            ("ctr_config",      ushort),    #rw
-            ("imt_config",      ushort),    #rw
-            ("cmp_config",      ushort),    #rw
+            ("io_config",       ushort),
+            # bits  0- 4  z-
+            # bit   5     rw  invert strobe input
+            # bit   6     rw  invert index input
+            #
+            # bit   7     rw  invert input B
+            # bit   8     rw  invert input A
+            #
+            # bit   9     rw  gate index using strobe
+            #
+            # bit  10     rw  (qd) swap A/B inputs (invert direction)
+            # bit  11     rw  (non-qd) count on:  0 both edges  1 rising edge
+            #
+            # bit  12     rw  compare-output to:  0 index pin  1 strobe pin
+            # bit  13     rw  compare-output enabled
+            #
+            # bits 14-15  rw  counter mode:
+            #                   0  quadrature decoding of A/B
+            #                   1  up(B=1)/down(B=0) counting on A
+            #                   2  up counting on A
+            #                   3  down counting on A
+            #
+            # In quadrature mode, the decoder issues up/down events based on
+            # transitions of inputs A/B.  Bit 11 has no effect in this mode.
+            # Bits 7, 8, and 10 all have equivalent function of inverting dir.
+            #
+            # The non-quadrature modes combine an event source:
+            #       0 << 11             any edge of input A
+            #       1 << 11 | 0 << 8    rising edge of input A
+            #       1 << 11 | 1 << 8    falling edge of input A
+            # with a direction:
+            #       1 << 14 | 0 << 7    down if B=0,  up   if B=1
+            #       1 << 14 | 1 << 7    up   if B=0,  down if B=1
+            #       2 << 14             up   regardless of B
+            #       3 << 14             down regardless of B
+            #
+            # Bit 10 has no effect in non-quadrature modes.
+
+            ("ctr_config",      ushort),
+            # bit   0     rw  watchdog timer enabled
+            # bit   1     rw  unit timer enabled
+            # bit   2     rw  latch imt on:  0 position read  1 unit period
+            # bit   3     rw  position counter enabled
+            # bits  4- 5  rw  latch position on index mode
+            #                   1 latch position on rising edge of index
+            #                   2 latch position on falling edge of index
+            #                   3 latch position on index marker
+            # bit   6     rw  latch position on strobe edge
+            #                   0 rising edge
+            #                   1 direction-dependent
+            # bit   7     rw  load position now (XXX does it auto-clear?)
+            # bit   8     rw  load position on index edge:  0 rising  1 falling
+            # bit   9     rw  load position on index enabled
+            # bit  10     rw  load position on strobe edge:  (see latch on strobe)
+            # bit  11     rw  load position on strobe enabled
+            # bits 12-13  rw  reset position on
+            #                   0 index event
+            #                   1 (disabled)
+            #                   2 first index event
+            #                   3 unit timer event
+            # bit  14     rw  emu soft suspend (counters run till zero)
+            # bit  15     rw  emu-free (bit 14 ignored)
+
+            ("imt_config",      ushort),
+            # bits  0- 3  rw  imt position event prescaler, log2 (0..11)
+            # bits  4- 6  rw  imt counter prescaler, log2
+            # bit  15     rw  imt enabled
+
+            ("cmp_config",      ushort),
+            # bits  0-11  rw  output pulse width (cycles / 4 - 1)
+            # bit  12     rw  position-compare enabled
+            # bit  13     rw  invert output
+            # bit  14     rw  load compare on  0 position zero  1 compare register
+            # bit  15     rw  load compare enabled
 
             #-------- status / event reporting ------------------------------------
             #
             ("irq",             EIrq),
-            ("status",          ushort),    #rc
+            # bit   0     (pending) irq active / (clear) eoi
+            #
+            # bit   1     position counter error
+            # bit   2     quadrature decoder error (A and B toggled at same time)
+            # bit   3     direction change event
+            # bit   4     watchdog timeout
+            # bit   5     position counter underflow
+            # bit   6     position counter overflow
+            # bit   7     compare load event
+            # bit   8     compare event
+            # bit   9     strobe latch event
+            # bit  10     index latch event
+            # bit  11     timer event
+
+            ("status",          ushort),
+            # bit   0     r-  position counter error (updated on index)
+            # bit   1     rc  first index event
+            # bit   2     rc  direction change event
+            # bit   3     rc  imt overflow event
+            # bit   4     r-  direction of last index
+            # bit   5     r-  direction of last position change
+            # bit   6     r-  direction of first index
+            # bit   7     rc  imt capture event
 
             #-------- interval measurement timer (aka "capture timer") ------------
             #
@@ -66,5 +158,6 @@ class EQep( ctypes.Structure ):
             #
             #   0x4_4d3_11_03  (v1.3.2 on subarctic 2.1)
         ]
+EQep.ld_compare = EQep.compare
 
 assert ctypes.sizeof(EQep) == 0x60
