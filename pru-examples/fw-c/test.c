80f66af30e497fe3b6de56c74a7be724cb4f620a
Matthijs van Duin <matthijsvanduin@gmail.com>
2020-07-06 20:42:14 +0200

    add comment to pru-examples/fw-c/test.c

--- pru-examples/fw-c/test.c
+++ pru-examples/fw-c/test.c
@@ -1,4 +1,7 @@
+// "far" will make the linker script place it in shared memory
 far int volatile foo = 41;
+// alternative declaration that allows more efficient access:
+//	far int volatile foo __attribute__((cregister("SHARED", near))) = 41;
 
 int main() {
 	foo++;
