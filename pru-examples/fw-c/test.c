82a402216000dbae95f63c45c21125c9b7f6ffa2
Matthijs van Duin <matthijsvanduin@gmail.com>
2020-07-07 05:48:25 +0200

    use fixed address for shmem variable in C example
    it's gross to assume the linker will place it where we expect it

--- pru-examples/fw-c/test.c
+++ pru-examples/fw-c/test.c
@@ -1,7 +1,8 @@
-// "far" will make the linker script place it in shared memory
-far int volatile foo = 41;
-// alternative declaration that allows more efficient access:
-//	far int volatile foo __attribute__((cregister("SHARED", near))) = 41;
+// Variables in the shared memory region (offset 0x10000 - 0x12fff in pruss) need to be
+// marked as "far".  The location attribute is used to set the exact address since the
+// python code (elf-test.py) needs to know where this variable is at.
+//
+far int volatile foo __attribute__((location(0x10000))) = 41;
 
 int main() {
 	foo++;
