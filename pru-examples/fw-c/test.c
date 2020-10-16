105129568758b1fb400e96fc9b29324663c68768
Matthijs van Duin <matthijsvanduin@gmail.com>
2020-10-16 03:58:32 +0200

    fw-c/test.c: use appropriate type for variable
    python maps it as uint32_t, and signed types should be avoided whenever
    possible on PRU since they are not natively supported.

--- pru-examples/fw-c/test.c
+++ pru-examples/fw-c/test.c
@@ -1,8 +1,10 @@
+#include <stdint.h>
+
 // Variables in the shared memory region (offset 0x10000 - 0x12fff in pruss) need to be
 // marked as "far".  The location attribute is used to set the exact address since the
 // python code (elf-test.py) needs to know where this variable is at.
 //
-far int volatile foo __attribute__((location(0x10000))) = 41;
+far uint32_t volatile foo __attribute__((location(0x10000))) = 41;
 
 int main() {
 	foo++;
