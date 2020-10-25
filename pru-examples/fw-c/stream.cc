a7e0318780b41d989a20fbddbdc6be214760b994
Matthijs van Duin <matthijsvanduin@gmail.com>
2020-10-25 19:39:43 +0100

    add pru-examples/stream-c.py + firmware
    shows streaming messages from C++ pru program via ringbfufer

---
+++ pru-examples/fw-c/stream.cc
@@ -0,0 +1,97 @@
+#include <stdint.h>
+#include <stddef.h>
+
+struct Message {
+	uint32_t id;
+};
+
+// layout of shared ddr3 memory (filled in by loader)
+struct DDRLayout {
+	Message volatile *msgbuf;
+	uint32_t num_msgs;
+};
+
+struct SharedVars {
+	// set by pru before halting
+	char const *abort_file;
+	int abort_line;
+
+	// read-pointer updated by python
+	uint32_t ridx;
+};
+
+far struct DDRLayout ddr __attribute__((location(0x10000))) = {};
+far struct SharedVars volatile shmem __attribute__((location(0x10100))) = {};
+
+
+// for easier debugging, record where in the source code we halted
+
+__attribute__((noreturn))
+void abort_at( char const *file, int line )
+{
+	shmem.abort_file = file;
+	shmem.abort_line = line;
+	for(;;) __halt();
+}
+
+static inline void assert_at( bool cond, char const *file, int line )
+{
+	if( ! cond )
+		abort_at( file, line );
+}
+
+#define abort() abort_at( __FILE__, __LINE__ )
+#define assert(cond) assert_at( (cond), __FILE__, __LINE__ )
+
+
+// local copy of write-pointer
+static uint32_t widx = 0;
+
+// global var for write-pointer is located right after message ringbuffer
+#define ddr_msgbuf_end	( ddr.msgbuf + ddr.num_msgs )
+#define ddr_widx	( *(uint32_t volatile *)ddr_msgbuf_end )
+
+void initialize()
+{
+	// perform sanity-checking
+	assert( 0x80000000 <= (uint32_t)ddr.msgbuf );
+	assert( ddr.msgbuf < ddr_msgbuf_end );
+
+	assert( ddr_widx == widx );
+	assert( shmem.ridx == widx );
+}
+
+static inline uint32_t next_idx( uint32_t idx )
+{
+	if( ++idx == ddr.num_msgs )
+		idx = 0;
+	return idx;
+}
+
+void send_message( uint32_t id )
+{
+	uint32_t next_widx = next_idx( widx );
+
+	if( next_widx == shmem.ridx ) {
+		// can't send message, ringbuffer is full
+		abort();
+	}
+
+	Message volatile *msg = &ddr.msgbuf[ widx ];
+
+	// fill in contents of message
+	msg->id = id;
+
+	// update write-pointer
+	ddr_widx = widx = next_widx;
+}
+
+void main() {
+	initialize();
+
+	uint32_t id = 0;
+	for(;;) {
+		send_message( ++id );
+		__delay_cycles( 100000 );
+	}
+}
---
+++ pru-examples/stream-c.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+from uio.utils import fix_ctypes_struct
+from uio.ti.icss import Icss
+import ctypes
+from ctypes import c_uint32 as u32
+from time import sleep
+
+pruss = Icss( "/dev/uio/pruss/module" )
+pruss.initialize()
+ddr = pruss.ddr
+
+core = pruss.core0
+core.load( 'fw-c/stream.out' )
+
+class Message( ctypes.Structure ):
+    _fields_ = [
+            ( 'id',     u32 ),
+        ]
+
+# used to communicate ddr layout to C program
+class DDRLayout( ctypes.Structure ):
+    _fields_ = [
+            ( 'msgbuf',      u32 ),
+            ( 'num_msgs',    u32 ),
+        ]
+
+# volatile variables in pruss shared memory
+class SharedVars( ctypes.Structure ):
+    _fields_ = [
+            ( 'abort_file', u32 ),
+            ( 'abort_line', u32 ),
+
+            ( 'ridx',       u32 ),
+        ]
+
+MSGBUF_OFFSET = 0
+NUM_MSGS = 1024
+
+# map shared memory variables
+ddr_layout = core.map( DDRLayout, 0x10000 )
+shmem = core.map( SharedVars, 0x10100 )
+msgbuf = ddr.map( Message * NUM_MSGS, MSGBUF_OFFSET )
+ddr_widx = ddr.map( u32, MSGBUF_OFFSET + ctypes.sizeof( msgbuf ) )
+
+# inform pru about layout of shared ddr memory region
+ddr_layout.msgbuf       = ddr.address + MSGBUF_OFFSET
+ddr_layout.num_msgs     = NUM_MSGS
+
+# local copy of read-pointer
+ridx = 0
+
+# initialize pointers in shared memory
+shmem.ridx = ridx
+ddr_widx.value = ridx
+
+# ready, set, go!
+core.run()
+
+lastid = 0
+
+while not core.halted:
+    while ridx != ddr_widx.value:
+        # note: it may be faster to copy a batch of messages from shared memory
+        # instead of directly accessing individual messages and their fields.
+        msg = msgbuf[ ridx ]
+
+        # check message id sequence to detect if message has been dropped
+        lastid = ( lastid + 1 ) & 0xffffffff
+        assert msg.id == lastid
+
+        # consume message and update read pointer
+        del msg
+        ridx += 1
+        if ridx == NUM_MSGS:
+            ridx = 0
+        shmem.ridx = ridx
+
+    print( f'\ridx=0x{ridx:04x} id=0x{lastid:08x} ', end='', flush=True )
+
+    sleep( 0.01 )
+
+abort_file = core.read( ctypes.c_char * 32, shmem.abort_file ).value  # FIXME
+
+print( f'core halted at {abort_file.decode("ascii")}:{shmem.abort_line}' )
+print( f'ridx       = {ridx}' )
+print( f'shmem.ridx = {shmem.ridx}' )
+print( f'ddr_widx   = {ddr_widx.value}' )
