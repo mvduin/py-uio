28a6632e9f90ab62207a12540e33b3cb585d3bb3
Matthijs van Duin <matthijsvanduin@gmail.com>
2020-11-06 19:56:27 +0100

    remove unhelpful comments

--- pru-examples/basic-test.py
+++ pru-examples/basic-test.py
@@ -7,20 +7,7 @@ pruss.initialize()
 
 core = pruss.core0
 
-# load program
 core.load( 'fw/test.bin' )
-
-# alternatively you can access iram as an array of instructions:
-#
-#       from ctypes import c_uint32 as uint
-#
-#       # map iram as array of words
-#       iram = core.iram.map( uint * 2048 )   # option one
-#       iram = core.iram.map().cast( 'I' )    # option two
-#
-#       iram[0] = 0x0101e0e0  # add r0, r0, 1
-#       iram[1] = 0x2a000000  # halt
-
 
 core.r0 = 123
 
