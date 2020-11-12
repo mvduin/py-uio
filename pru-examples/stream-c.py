a77747da2459d0dcf5e77e96465010334bd93ccd
Matthijs van Duin <matthijsvanduin@gmail.com>
2020-11-12 03:49:38 +0100

    pru-examples/stream-c: don't crash if ddr region is too large

--- pru-examples/stream-c.py
+++ pru-examples/stream-c.py
@@ -40,11 +40,10 @@ class SharedVars( ctypes.Structure ):
 MSGBUF_OFFSET = 0
 
 # you can use a fixed ringbuffer size:
-NUM_MSGS = 1024
-# you can scale the ringbuffer to fit the size of the ddr region:
+#NUM_MSGS = 1024
+# or you can scale the ringbuffer to fit the size of the ddr region:
 NUM_MSGS = ( ddr.size - MSGBUF_OFFSET - ctypes.sizeof( u16 ) ) // ctypes.sizeof( Message )
-
-assert NUM_MSGS < 65536
+NUM_MSGS = min( NUM_MSGS, 65535 )  # make sure number of messages fits in u16
 
 # map shared memory variables
 ddr_layout = core.map( DDRLayout, 0x10000 )
