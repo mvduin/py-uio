ad9d0fc739233dd6632ecfef300783371801991a
Matthijs van Duin <matthijsvanduin@gmail.com>
2022-11-09 01:09:14 +0100

    update gpio.h header
    add GPIO_DIR register definition with appropriate warning
    
    update comments

--- pru-examples/fw/gpio.h
+++ pru-examples/fw/gpio.h
@@ -6,9 +6,17 @@
 #define GPIO2	0x481ac100
 #define GPIO3	0x481ae100
 
-// To avoid race conditions with code running on the cortex-a8, do not write
-// directly to GPIO_OUT.  Use the SET/CLEAR registers instead.
-#define GPIO_IN		0x38  //r-  received levels
-#define GPIO_OUT	0x3c  //rw  output levels (if output enabled)
+// WARNING:  if you need PRU to change the direction of GPIOs, make absolutely
+// sure to not simultaneously use linux to change the direction of other GPIOs
+// belonging to the same GPIO controller, since this incurs a race condition.
+//
+// Similarly, to avoid race conditions with linux, avoid writing directly to
+// the GPIO_OUT register, use the GPIO_SET/CLEAR registers instead.
+
+// For each of these registers, the 32 bits of a register represent each of
+// the 32 GPIOs of the GPIO controller you're accessing.
+#define GPIO_DIR	0x34  //rw  direction: 0 = output, 1 = input
+#define GPIO_IN		0x38  //r-  input levels
+#define GPIO_OUT	0x3c  //rw  output levels (if direction is output)
 #define GPIO_CLEAR	0x90  //-s  write 1 to set output low
 #define GPIO_SET	0x94  //-c  write 1 to set output high
